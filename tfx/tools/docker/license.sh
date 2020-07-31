42cc86a512fb0345063de407ae7cf76c2a062a5c
jiyongjung <jiyongjung@google.com>
2020-07-30 22:48:37 -0700

    Reads LICENSE from locally available files and github license API to reduce the burden of maintaining third party licenses.
    Reads third party LICENSE files from local environment by default. If not found, try guessing the url based on the homepage url. Still uses third_party_licenses.csv as a last resort.
    Also uses license information of the package to identify GPL-licensed packages.
    
    FYI, number of packages according to the source of licenses.
    - locally available: 68
    - using CSV: 45
    - github url guessing: 29
    
    Most of remaining entries have its dedicated home page other than github, and I couldn't find any more clues to connect them with license files any more.
    
    PiperOrigin-RevId: 324155266

--- tfx/tools/docker/license.sh
+++ tfx/tools/docker/license.sh
@@ -14,17 +14,23 @@
 # limitations under the License.
 
 
-# A script to gather locally-installed python packages, and based on
-# a specified license table (3 columns: name,license_link,license_type) csv file,
-# download and save all license files into specified directory.
+# A script to gather licenses of locally-installed python packages.
+# There are three sources of licenses
+# - Locally available LICENSE files which are installed with the package.
+# - A license table (3 columns: name,license_link,license_type) csv file
+#   which can be used to download license files.
+# - If the homepage of the package is on github, try with github URL pattern.
+# These license files will be copied into specified directory.
 # Usage:
 #   license.sh third_party_licenses.csv /usr/licenses
 
+set -e
+set +x
 
 # Get the list of python packages installed locally.
+OLD_IFS="$IFS"
 IFS=$'\n'
-INSTALLED_PACKAGES=($(pip freeze --exclude-editable | grep -v "pkg-resources" | sed s/=.*//))
-
+INSTALLED_PACKAGES_WITH_VERSION=($(pip freeze --exclude-editable | grep -v "pkg-resources"))
 
 # Get the list of python packages tracked in the given CSV file.
 REGISTERED_PACKAGES=()
@@ -32,43 +38,87 @@ while IFS=, read -r col1 col2 col3
 do
   REGISTERED_PACKAGES+=($col1)
 done < $1
+IFS="$OLD_IFS"
 
-while IFS=, read -r col1 col2 col3
-do
-  echo "Obtaining installed packages and licenses:"
-  for j in "${INSTALLED_PACKAGES[@]}"; do
-    if [[ $j == $col1 ]]; then
-      echo "${col1},${col2},${col3}"
-    fi
-  done
-done < $1
+mkdir -p $2
 
-# Make sure all locally installed packages are covered.
-DIFF=()
-for i in "${INSTALLED_PACKAGES[@]}"; do
+GPL_PACKAGES=()
+REMOTE_LICENSE_PACKAGES=()
+# Copy locally available LICENSE files and gather packages without them.
+for i in "${INSTALLED_PACKAGES_WITH_VERSION[@]}"; do
+  # `i` looks like "pkg_name==0.0.1".
+  pkg=${i%==*}
+  pkg_base_dir=$(python -m pip show ${pkg} | sed -n 's/^Location: //p')
+  # LICENSE files are located under .../site-packages/pkg_name-0.0.1.dist-info/.
+  pkg_dist_info_dir="${pkg_base_dir}/${i/==/-}.dist-info"
+  set +e
+  license_files=$(ls ${pkg_dist_info_dir}/LICENSE* 2> /dev/null)
+  if [[ $? -ne 0 ]]; then
+    REMOTE_LICENSE_PACKAGES+=("$pkg")
+  else
+    echo "License found: ${license_files}"
+    cat ${license_files} > $2/$pkg.LICENSE
+  fi
+  set -e
+
+  license=$(python -m pip show ${pkg} | sed -n 's/^License: //p')
+  if [[ "${license}" == *GPL* ]]; then
+    GPL_PACKAGES+=("$pkg")
+  fi
+done
+
+# Collect missing packages.
+NOT_REGISTERED_PACKAGES=()
+for i in "${REMOTE_LICENSE_PACKAGES[@]}"; do
   skip=
   for j in "${REGISTERED_PACKAGES[@]}"; do
     # PyPI package name is case-insensitive so cast to lower case.
     [[ ${i,,} == ${j,,} ]] && { skip=1; break; }
   done
-  [[ -n $skip ]] || DIFF+=("$i")
+  [[ -n $skip ]] || NOT_REGISTERED_PACKAGES+=("$i")
 done
 
-if [ -n "$DIFF" ]; then
+# Download licenses from the url in the csv file.
+while IFS=, read -r col1 col2 col3
+do
+  if [[ " ${REMOTE_LICENSE_PACKAGES[@]} " =~ " ${col1} " ]]; then
+    echo "Downloading license for ${col1} from ${col2}"
+    curl --fail -sSL -o $2/$col1.LICENSE $col2
+  fi
+done < $1
+IFS="$OLD_IFS"
+
+# Try guessing with github URL
+MISSING=()
+for pkg in "${NOT_REGISTERED_PACKAGES[@]}"; do
+  homepage=$(python -m pip show ${pkg} | sed -n 's/^Home-page: //p')
+  if [[ "${homepage}" == *github.com/* ]]; then
+    repo_name=${homepage#*github.com/}
+    set +e
+    curl --fail -sSL -o $2/$pkg.LICENSE \
+      "https://raw.githubusercontent.com/${repo_name}/master/LICENSE" \
+      2> /dev/null
+    if [[ $? -ne 0 ]]; then
+      MISSING+=("$pkg")
+    else
+      echo "Downloaded license for ${pkg} by guessing github URL: ${repo_name}"
+    fi
+    set -e
+  else
+    MISSING+=("$pkg")
+  fi
+done
+
+if [ -n "$MISSING" ]; then
   echo "The following packages are not found for licenses tracking."
   echo "Please add an entry in $1 for each of them."
-  echo ${DIFF[@]}
+  echo ${MISSING[@]}
   exit 1
 fi
 
-# Gather license files for each package. For packages with GPL license we mirror the source code.
+# Download source code for GPL packages.
 mkdir -p $2/source
-while IFS=, read -r col1 col2 col3
-do
-  if [[ " ${INSTALLED_PACKAGES[@]} " =~ " ${col1} " ]]; then
-    wget -O $2/$col1.LICENSE $col2
-    if [[ "${col3}" == *GPL* ]]; then
-      pip install -t "$2/source/${col1}" ${col1}
-    fi
-  fi
-done < $1
+for i in "${GPL_PACKAGES[@]}"; do
+  echo "Downloading source of the GPL-licensed package: ${i}"
+  python -m pip install -t "$2/source/${i}" ${i}
+done
